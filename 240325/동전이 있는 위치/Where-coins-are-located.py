03bdfd61e821effbf18616b9687633aaa0ff4974
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-03-25 16:34:10 +0900

    Title: 동전이 있는 위치, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240325/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,12 @@
+n, m = map(int,input().split())
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(m):
+    pos = list(map(int,input().split()))
+    arr[pos[0]-1][pos[1]-1] = 1
+
+for i in arr:
+    for j in i:
+        print(j, end = " ")
+    print()
