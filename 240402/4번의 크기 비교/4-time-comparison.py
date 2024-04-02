f905e5f9eb2912a6316c25bdb4f19161e375afbf
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-02 13:15:42 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,19 @@
+a = int(input())
+a,b,c,d = map(int,intput().split())
+
+if a>b:
+    print(1)
+else:
+    print(0)
+if a>c:
+    print(1)
+else:
+    print(0)
+if a>d:
+    print(1)
+else:
+    print(0)
+if a>e:
+    print(1)
+else:
+    print(0)
