3b9bd8ce044af9158780bfb5d1143cc8464a2e13
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-02 17:10:31 +0900

    Title: 배열 채우기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240402/배열 채우기/filling-array.py
+++ 240402/배열 채우기/filling-array.py
@@ -1,5 +1,10 @@
 arr = list(map(int,input().split()))
+cnt = 0
+for i in arr:
+    if i == 0:
+        break
+    cnt += 1
 
-for i in range(len(arr)-1,-1,-1):
+for i in range(cnt-1,-1,-1):
     if arr[i] != 0:
         print(arr[i], end = " ")
