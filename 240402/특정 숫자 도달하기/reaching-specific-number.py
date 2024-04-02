4ac4f0a2001aed6aad360a49ce28b26f7d50580c
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-02 16:52:10 +0900

    Title: 특정 숫자 도달하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/특정 숫자 도달하기/reaching-specific-number.py
+++ 240402/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,3 +1,10 @@
 a = list(map(int,input().split()))
+asum = 0
+cnt = 0
+for i in a:
+    if i >= 250:
+        break
+    asum += i
+    cnt +=1
 
-print("%d %.1f" %(sum(a), sum(a)/10))
+print("%d%.1f"%(asum, asum/cnt))
