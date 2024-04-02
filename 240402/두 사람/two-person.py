907b4a50bcc71590c5c56526684e8b672eba85fc
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-02 13:23:58 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/두 사람/two-person.py
@@ -0,0 +1,10 @@
+p1 = input().split()
+p2 = input().split()
+result = False
+
+if int(p1[0]) >= 19 and p1[1] = 'M':
+    result = True
+elif int(p2[0]) >= 19 and p2[1] = 'M':
+    result = True
+if result == True:
+    print(1)
