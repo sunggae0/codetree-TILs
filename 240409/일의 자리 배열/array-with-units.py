e9fad094c84f3414d7e15f985761f8ba8fe95980
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-09 13:32:59 +0900

    Title: 일의 자리 배열, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240409/일의 자리 배열/array-with-units.py
+++ 240409/일의 자리 배열/array-with-units.py
@@ -4,6 +4,8 @@ arr[0], arr[1] = map(int,input().split())
 
 for i in range(2,10):
     arr[i] = arr[i-1] + arr[i-2]
+    if arr[i] >= 10:
+        arr[i] = arr[i]%10
 
 for i in arr:
     print(i,end=" ")
