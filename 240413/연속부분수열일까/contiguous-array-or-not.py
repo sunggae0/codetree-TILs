4bb54647167d837c75b9d8910c25e79efd4a0aaf
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-13 21:34:50 +0900

    Title: 연속부분수열일까, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240413/연속부분수열일까/contiguous-array-or-not.py
+++ 240413/연속부분수열일까/contiguous-array-or-not.py
@@ -4,12 +4,11 @@ a = list(map(int,input().split()))
 b=  list(map(int,input().split()))
 
 p = 0
-while p<= len(a)-len(b):
+while p<= n1-n2:
     result = True
-    for i in range(p,p+len(b)):
+    for i in range(p,p+n2):
         if a[i] != b[i-p]:
             result = False
-            break
     p += 1
 
 if result == True:
