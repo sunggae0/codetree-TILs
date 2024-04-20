8746ce30c7141e6bb9934e4a8025047b4f2e79f2
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-04-20 23:46:53 +0900

    Title: 숫자로 이루어진 사각형, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 240420/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 240420/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -1,9 +1,13 @@
 n = int(input())
 
 def sqr(n):
+    cnt = 0
     for i in range(n):
         for j in range(n):
-            print(j+4*i, end = ' ')
+            if cnt < 9:
+                cnt+=1
+            else:
+                cnt = 1
+            print(cnt, end=' ')
         print()
-
 sqr(n)
