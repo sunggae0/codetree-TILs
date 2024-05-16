ae318e5d7796c2cebc0dd9b80e189e546e4a4f5e
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-16 23:26:49 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240516/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240516/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,12 +1,22 @@
-n = int(input())
-arr = list(map(int,input().split()))
-def f(cnt):
-    result = True
-    for i in arr:
-        if cnt % i != 0:
-            result = False
-    if result:
-        return cnt
-    return f(cnt+min(arr))
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
 
-print(f(max(arr)))
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+def lcm_of_list(nums, n):
+    if n == 1:
+        return nums[0]
+    else:
+        return lcm(nums[n-1], lcm_of_list(nums, n-1))
+
+# 입력 처리
+n = int(input().strip())
+numbers = list(map(int, input().strip().split()))
+
+# 최소공배수 계산
+result = lcm_of_list(numbers, n)
+print(result)
