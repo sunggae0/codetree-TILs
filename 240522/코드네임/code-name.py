0b1bc6fa18e4ed8684241b5a190fbc816e43c885
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-22 23:14:48 +0900

    Title: 코드네임, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240522/코드네임/code-name.py
@@ -0,0 +1,14 @@
+class p():
+    def __init__(self,code,point):
+        self.code = code
+        self.point = point
+    
+arr = [p(*(input().split())) for _ in range(5)]
+
+minn = 0
+for i in range(5):
+    pt = int(arr[i].point)
+    if pt < minn and i!=0:
+        minn = i
+    
+print(arr[minn].code, arr[minn].point)
