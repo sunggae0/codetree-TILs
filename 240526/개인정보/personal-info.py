ec94a4613368f4c5b1c13007c3ed01cdba901862
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-26 22:32:41 +0900

    Title: 개인정보, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240526/개인정보/personal-info.py
@@ -0,0 +1,17 @@
+class std():
+    def __init__(self,name,height,weight):
+        self.height = height
+        self.weight = weight
+        self.name = name
+
+arr = [std(*input().split()) for i in range(5)]
+
+temp = sorted(arr, key = lambda x: (x.name))
+print('name')
+for i in temp:
+    print(i.name, i.height, i.weight,)
+
+temp = sorted(arr, key = lambda x: (x.height))
+print('height')
+for i in temp:
+    print(i.name, i.height, i.weight,)
