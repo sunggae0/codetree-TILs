d50fc3bb0523f759b29852847b7ece5c02884508
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-28 16:13:19 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240528/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,15 @@
+def find_min(arr):
+    for i in range(n):
+        if arr[i] == min(arr):
+            return i
+
+n = int(input())
+arr = list(map(int,input().split()))
+indx = [0 for _ in range(n)]
+
+for i in range(n):
+    a = find_min(arr)
+    indx[a] = i+1
+    arr[a] = 1001
+
+print(*indx,sep = ' ')
