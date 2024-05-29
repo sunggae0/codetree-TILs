b61aea6de368b6dcbdc92690c5db4db01863ac23
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-29 23:53:48 +0900

    Title: 줄 세우기 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240529/줄 세우기 2/line-up-students-2.py
+++ 240529/줄 세우기 2/line-up-students-2.py
@@ -1,38 +1,29 @@
-def sort_students():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    
-    N = int(data[0])
+class Student:
+    def __init__(self, height, weight, id):
+        self.height = height
+        self.weight = weight
+        self.id = id
+
+    def __repr__(self):
+        return f"{self.height} {self.weight} {self.id}"
+
+def sort_students(student_data):
     students = []
-
-    index = 1
-    for i in range(N):
-        h = int(data[index])
-        w = int(data[index + 1])
-        students.append((h, w, i + 1))
-        index += 2
     
-    # Sort by height first, and by weight in reverse order second
-    students.sort(key=lambda x: (x[0], -x[1]))
-
+    # 학생 정보 객체를 생성하고 리스트에 추가
+    for i, (h, w) in enumerate(student_data):
+        students.append(Student(h, w, i + 1))
+    
+    # 정렬: 키 오름차순, 몸무게 내림차순
+    students.sort(key=lambda student: (student.height, -student.weight))
+    
+    # 정렬된 학생 정보 출력
     for student in students:
-        print(student[0], student[1], student[2])
+        print(student)
 
-# Test the function with sample input
-from io import StringIO
-import sys
+# 입력 데이터: 각 튜플은 (키, 몸무게)를 나타냄
+n = int(input())
+input_data = [tuple(map(int,input().split())) for _ in range(n)]
 
-# Sample input
-input_data = """4
-1 5
-1 7
-3 6
-1 10
-"""
-
-# Redirect stdin to the sample input
-sys.stdin = StringIO(input_data)
-
-# Run the function
-sort_students()
+# 함수 실행
+sort_students(input_data)
