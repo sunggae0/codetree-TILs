55efcc09a636689abc6cd8cb8f6cc0fd1545dbe6
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-31 14:50:33 +0900

    Title: DateTime to DateTime, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240531/DateTime to DateTime/datetime-to-datetime.py
+++ 240531/DateTime to DateTime/datetime-to-datetime.py
@@ -1,15 +1,4 @@
 a2,b2,c2 = map(int,input().split())
 a1,b1,c1 = 11,11,11
-cnt = 0
-while True:
-    if a1==a2 and b1==b2 and c1==c2:
-        break
-    c1 += 1
-    cnt += 1
-    if c1==60:
-        b1+=1
-        c1= 0
-    if b1 == 24:
-        a1+= 1
-        b1=0
-print(cnt)
+
+print((c2-c1)+(b2-b1)*60+(a2-a1)*60*24)
