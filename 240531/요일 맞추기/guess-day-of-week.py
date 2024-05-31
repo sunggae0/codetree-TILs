9f9fda9d6980dec618a70aec8b55f472370e7cec
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-31 15:18:11 +0900

    Title: 요일 맞추기, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240531/요일 맞추기/guess-day-of-week.py
+++ 240531/요일 맞추기/guess-day-of-week.py
@@ -1,7 +1,7 @@
 def days(m2,d2):
-    m1,d1=1,1
+    m1,d1 = 1,1
     cnt = 0
-    while m1!=m2 and d2!=d1:
+    while m1!=m2 or d2!=d1:
         d1+=1
         if d1 == m[m2]:
             m1+=1
@@ -15,24 +15,9 @@ m = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 d=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
 
 d_num=1
+days1 = days(m1,d1)
+days2 = days(m2,d2)
 
-if days(m1,d1) > days(m2,d2):
-    while True:
-        if m1==m2 and d1==d2:
-            break
-        d1-=1
-        d_num-=1
-        if d1==m[m1]:
-            m1-=1
-            d1=1
-else:
-    while True:
-        if m1==m2 and d1==d2:
-            break
-        d1+=1
-        d_num+=1
-        if d1==m[m1]:
-            m1+=1
-            d1=1
+d_num += days2 - days1
 
 print(d[d_num%7])
