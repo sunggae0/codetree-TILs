556450b362f22b328454b8c938104cbcb8db98d0
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-05-31 16:29:19 +0900

    Title: 그 요일은, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240531/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,21 @@
+def days(m2,d2):
+    m1,d1,cnt = 1,1,0
+    while m1!=m2 or d1!=d2:
+        d1 += 1
+        cnt+=1
+        if d1 > m[m1]:
+            m1+=1
+            d1=1
+    return cnt
+
+m1,d1,m2,d2 = map(int,input().split())
+a = input()
+m = [31,29,31,30,31,30,31,31,30,31,30,31]
+d=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+
+selected_day = d.index(a) + 1
+cnt=0
+for i in range(days(m1,d1),days(m2,d2)+1):
+    if i%selected_day==0:
+        cnt+=1
+print(cnt)
