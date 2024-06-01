05005be1c11034655f3d7a0e384c8e80e58c6c6d
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-06-01 23:50:26 +0900

    Title: 그 요일은, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240601/그 요일은/the-day-of-the-day.py
+++ 240601/그 요일은/the-day-of-the-day.py
@@ -1,46 +1,38 @@
-def days(m2,d2):
-    m1,d1,cnt = 1,1,0
-    while True:
-        if m1==m2 and d1==d2:
-            break
-        d1 +=1
-        cnt += 1
-        if d1 > m[m1]:
-            d1=1
-            m1+=1
-    return cnt
+def days_since_start_of_year(month, day):
+    """1월 1일부터 해당 월의 날짜까지의 총 일수를 계산하는 함수"""
+    month_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    for i in range(1, month):
+        total_days += month_days[i]
+    total_days += day - 1
+    return total_days
 
-m = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-d = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+def count_specific_weekday(m1, d1, m2, d2, start_day, target_day):
+    """특정 요일의 빈도를 계산하는 함수"""
+    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    start_idx = weekdays.index(start_day)
+    target_idx = weekdays.index(target_day)
 
-m1,d1,m2,d2 = map(int,input().split())
-selected = d.index(input())
+    # 시작 날짜의 요일 계산
+    start_days = days_since_start_of_year(m1, d1)
+    start_weekday = (start_days + start_idx) % 7
 
-day1 = days(m1,d1)
-day2 = days(m2,d2)
+    # 끝 날짜의 요일 계산
+    end_days = days_since_start_of_year(m2, d2)
+    end_weekday = (end_days + start_idx) % 7
 
-cnt=0
-for i in range(day1,day2+1):
-    if i%7 == selected:
-        cnt+=1
-print(cnt)
-'''
+    # 특정 요일의 빈도 계산
+    count = 0
+    for day_count in range(start_days, end_days + 1):
+        if (day_count % 7) == target_idx:
+            count += 1
+    
+    return count
 
-def days(m2,d2):
-    m1, d1, cnt = 1, 1, 0
-    while True:
-        if m1 == m2 and d1 == d2:
-            break
-        d1 += 1
-        cnt += 1
-        if d1 > m[m1]:
-            d1 = 1
-            m1 += 1
-    return cnt
+# 입력 받기
+m1, d1, m2, d2 = map(int, input().split())
+start_day = input().strip()
 
-# 월별 일수, 윤년을 가정하며 2월은 29일
-m = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-# 2월 5일까지의 일수 계산
-print(days(2, 5))  # 예상 출력: 1월 31일 + 2월 4일 = 35일
-'''
+# 결과 출력
+result = count_specific_weekday(m1, d1, m2, d2, start_day, start_day)
+print(result)
