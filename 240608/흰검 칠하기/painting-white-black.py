f846b5b98f40955e888e8d7f1cf1f54ec02efdf8
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-06-08 23:59:41 +0900

    Title: 흰검 칠하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240608/흰검 칠하기/painting-white-black.py
+++ 240608/흰검 칠하기/painting-white-black.py
@@ -1,57 +1,49 @@
-def refresh_list(arr,move):
-    result = [[] for _ in range(move)] + arr
-    return result
+def refresh_list(arr, move):
+    return [[] for _ in range(move)] + arr
 
-
-def count(var,arr):
-    cnt = 0
-    for i in arr:
-        if i==var:
-            cnt+=1
-    return cnt
-
-
+def count(var, arr):
+    return arr.count(var)
 
 n = int(input())
-arr=[]
+arr = []
 offset = 0
-pos=0
+pos = 0
 color = 0
+
 for i in range(n):
-    x,comm = input().split()
-    x=int(x)
+    x, comm = input().split()
+    x = int(x)
 
-    if comm=='R':
-        temp = [pos,pos+x]
-        for j in range(x):
-            arr.append([])
-        pos+=x
-    elif comm=='L':
-        temp = [pos-x,pos]
-        pos-=x
-    
-    if pos<0:
-        move = pos*(-1)
+    if comm == 'R':
+        temp = [pos, pos + x]
+        pos += x
+    elif comm == 'L':
+        temp = [pos - x, pos]
+        pos -= x
+
+    if pos < 0:
+        move = -pos
         offset += move
-        pos=0
-        temp = [0,x]
-        arr = refresh_list(arr,move)
-    
-    for j in range(temp[0],temp[1]):
-        if arr[j] and arr[j].count(0)>=2 or arr[j].count(1) >= 2:
-            arr[j].append(2) # gray
-        else:
-            arr[j].append(color%2) #even=black, odd=white
-            
-    color+=1
+        pos = 0
+        temp = [0, temp[1] + move]
+        arr = refresh_list(arr, move)
 
-cnt=[0,0,0]
-for i in arr:
-    if len(i)>0:
-        if i[-1] == 2:
-            cnt[2]+=1
-        elif i[-1] == 1:
-            cnt[0]+=1
+    for j in range(temp[0], temp[1]):
+        if j >= len(arr):
+            arr.append([])
+        if count(0, arr[j]) >= 2 and count(1, arr[j]) >= 2:
+            arr[j].append(2)  # gray
         else:
-            cnt[1]+=1
-print(*cnt,sep=' ')
+            arr[j].append(color % 2)  # even=black, odd=white
+    color += 1
+
+cnt = [0, 0, 0]
+for i in arr:
+    if len(i) > 0:
+        if i[-1] == 2:
+            cnt[2] += 1
+        elif i[-1] == 1:
+            cnt[0] += 1
+        else:
+            cnt[1] += 1
+print(*cnt, sep=' ')
