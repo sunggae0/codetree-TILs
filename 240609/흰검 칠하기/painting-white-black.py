2251692101e329972523f6f20880f79a2dd7974f
sunggae0 <163130858+sunggae0@users.noreply.github.com>
2024-06-09 23:13:37 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240609/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,53 @@
+def refresh_list(arr,move):
+    result = [[] for _ in range(move)] + arr
+    return result
+
+def count(var,arr):
+    cnt=0
+    for i in arr:
+        if i == var:
+            cnt+=1
+    return cnt
+
+
+
+
+n = int(input())
+arr=[]
+offset = 0
+pos=0
+color=1
+for i in range(n):
+    x,comm = input().split()
+    x=int(x)
+
+    if comm=='R':
+        temp = [pos,pos+x]
+        if pos+x+1>len(arr):
+            for j in range(pos+x-len(arr)):
+                arr.append([])
+        pos+=x-1
+    elif comm=='L':
+        temp = [pos-x+1,pos+1]
+        pos-=x-1
+    
+    if pos<0:
+        move = pos*(-1)
+        offset += move
+        pos=0
+        temp = [0,temp[1]+move]
+        arr = refresh_list(arr,move)
+    for j in range(temp[0],temp[1]):
+        if len(arr[j]) > 0:
+            if arr[j][-1] != 2:
+                arr[j].append(color%2) #1 = black, 0 = white, 2 = gray
+                if count(0,arr[j])>=2 and count(1,arr[j])>=2:
+                    arr[j].append(2)
+        else:
+            arr[j].append(color%2) #1 = black, 0 = white, 2 = gray
+    color += 1
+
+cnt=[0,0,0]
+for i in arr:
+    cnt[i[-1]]+=1
+print(*cnt,sep=' ')
